786935126ab343ebb14c4bf226b57f761357f2be
Ahmed Hussein <ahmedhussein6199@gmail.com>
2024-05-07 17:16:30 +0300

    cleaning up the code

--- SolarTracker/SolarTracker.ino
+++ SolarTracker/SolarTracker.ino
@@ -1,23 +1,23 @@
 #include <IRremote.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-// print hello world
-//define IR receiver pin
-#define ir_pin 3  //Analog
 
-//define motor pins
+
+#define ir_pin 3  //IR receiver Pin
+
+//Motor Pins
 #define MotorH_pin1 2
 #define MotorH_pin2 4
 #define MotorH_enable 5  //Analog
 #define MotorV_pin1 7
 #define MotorV_pin2 8
 #define MotorV_enable 6  //Analog
-#define LDRV A2         //Analog
-#define LDRH A3           //Analog
+#define LDRV A2          //Analog
+#define LDRH A3          //Analog
 
 //define remote buttons
-#define button1 0xBA45FF00 //auto
-#define button2 0xB946FF00 //manual
+#define button1 0xBA45FF00  //Auto
+#define button2 0xB946FF00  //Manual
 #define buttonUp 0xE718FF00
 #define buttonDown 0xAD52FF00
 #define buttonRight 0xA55AFF00
@@ -27,7 +27,7 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 IRrecv receiver(ir_pin);
 
-
+//Functions declaration
 void MotorUp();
 void MotorDown();
 void MotorRight();
@@ -37,11 +37,16 @@ void IRreceiver();
 void SpeedControl(int value);
 void printVolts();
 void lcdupdate(String msg);
+
+//Global variables
 int speed = 170;
 int delayy = 50;
 unsigned long key_value = 0;
 bool auto_mode = false;
 
+
+
+//main code
 void setup() {
   Serial.begin(9600);
   receiver.enableIRIn();  // Enable IR reception
@@ -54,35 +59,33 @@ void setup() {
   digitalWrite(MotorH_pin2, LOW);
 }
 
-
 void loop() {
-
   IRreceiver();
-  // printVolts();
+  printVolts();
 }
 
+////////////////
 void IRreceiver() {
   if (receiver.decode()) {
     long code = receiver.decodedIRData.decodedRawData;
-    //Serial.println("Code");
     Serial.println(code, HEX);
-    if (!code){
+    if (!code) {
       code = key_value;
       speed += 10;
       delayy += 20;
       Serial.println(speed);
-      }
-      else{
-        speed = 170;
-        delayy = 20;
-      }
-    //Serial.println("KeyValue");
+    } else {
+      speed = 170;
+      delayy = 20;
+    }
     //Serial.println(code, HEX);
     if (code == button1) {
       Serial.println("AutoMode On");
+      lcdupdate("Auto Mode On");
       auto_mode = true;
     } else if (code == button2) {
       Serial.println("ManualMode On");
+      lcdupdate("Manual Mode On");
       speed = 170;
       delayy = 50;
       auto_mode = false;
@@ -95,7 +98,8 @@ void IRreceiver() {
     Auto_mode();
   }
 }
-//////////////////
+
+// Motor Movement Functions
 void MotorUp() {
   analogWrite(MotorV_enable, speed);
   digitalWrite(MotorV_pin1, HIGH);
@@ -126,6 +130,8 @@ void MotorLeft() {
   digitalWrite(MotorH_pin2, LOW);
 }
 
+
+//LCD screen functions
 void printVolts() {
   int sensorValue = analogRead(A0);
   float Voltage = sensorValue * (12.0 / 1023.0);  // Convert the sensor value to true voltage
@@ -133,14 +139,15 @@ void printVolts() {
   lcd.print("Voltage =");
   lcd.print(Voltage);  // Print the Voltage on LCD
 }
-void lcdupdate(String msg){
-    lcd.clear();
-    lcd.print(msg);
-    delay(1500);
-    lcd.clear();
-    printVolts();
+void lcdupdate(String msg) {
+  lcd.clear();
+  lcd.print(msg);
+  delay(500);
+  lcd.clear();
+  printVolts();
 }
 
+//Manual Controls
 void Manual_mode(long code) {
   switch (code) {
     case buttonUp:
@@ -168,40 +175,34 @@ void Manual_mode(long code) {
 }
 
 void Auto_mode() {
-
   int v_value = analogRead(LDRV);
   int h_value = analogRead(LDRH);
-
-  
-  Serial.print("v_value: ");
-  Serial.println(v_value);
-  SpeedControl(v_value); 
-  Serial.print("speed: ");
-  Serial.println(delayy);
+  // Serial.print("v_value: ");
+  // Serial.println(v_value);
+  // SpeedControl(v_value);
+  // Serial.print("speed: ");
+  // Serial.println(delayy);
   if (v_value < 250) {
     MotorUp();
   } else if (v_value > 750) {
     MotorDown();
   }
+
   Serial.print("h_value: ");
   Serial.println(h_value);
   SpeedControl(h_value);
 
- 
   if (h_value < 250) {
     MotorLeft();
-    
   } else if (h_value > 750) {
     MotorRight();
-     
   }
 }
-
 void SpeedControl(int value) {
   int x = abs(value - 511);
   if (x < 150)
     speed = 0;
-  else if ( x < 250)
+  else if (x < 250)
     speed = 50;
   else if (x < 350)
     speed = 130;
